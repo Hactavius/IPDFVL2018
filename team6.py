d8a7f3a07610b50c74ae50c6ca63a6be595ee038
Jackson Fleming <flemijac000@students.fvlhs.org>
2018-11-06 19:34:27 +0000

    Upload team6 again

--- team6.py
+++ team6.py
@@ -5,19 +5,38 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
-team_name = 'MasterChef'
-strategy_name = 'Juke'
-strategy_description = 'Checks past history and adjusts accordingly.'
-betrays = 0
+import random
+team_name = 'Backstab'
+strategy_name = 'Looks peaceful but it hurts.'
+strategy_description = 'How does this strategy decide?'
+betraytime = 0
+didb = 0
+bc = 0
 def move(my_history, their_history, my_score, their_score):
-    global betrays
+    global betraytime
+    global didb
+    global bc
     if len(my_history) == 0:
         return 'c'
-    if their_history[-1] == 'b':
-        betrays += 1
-    if their_history[-1] == 'b':
-        for g in range(betrays):
-            return 'b'
-    elif their_history[-1] == 'c':
+    if len(my_history) == 1:
         return 'c'
+    if betraytime == 4:
+        betraytime = 0
+        return 'b'
+    elif their_history[-1] == 'c':
+        betraytime =betraytime + 1
+        return 'c'
+    elif their_history[-1]== 'b':
+        didb += 1
+        betraytime =betraytime +1
+        if didb == 1:
+            betraytime = betraytime +1
+            return 'c'
+        elif didb == 2:
+            didb = 0
+            betraytime =betraytime +1
+            bc +=1
+            if bc == 3:
+                bc = 0
+                return 'b'
+            return 'b'
